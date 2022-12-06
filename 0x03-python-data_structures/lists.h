d8334fc9a3b862af5a92038f2adefb8f90566a4c
emmanuel tofa <emmanuelutofa@gmail.com>
2022-12-06 09:22:32 +0000

    Print a list of integers

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,5 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
+#define ARRSIZE 256
+#include <stddef.h>
 
 /**
  * struct listint_s - singly linked list
@@ -7,19 +9,19 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ * for project
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
+int *to_array(listint_t *head, size_t *length);
 void free_listint(listint_t *head);
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+
 int is_palindrome(listint_t **head);
-size_t listint_len(const listint_t *h);
 
 #endif /* LISTS_H */
