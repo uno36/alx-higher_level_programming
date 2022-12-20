1a6c90133e77d563fc08c2ca3604ea718c391245
emmanuel tofa <emmanuelutofa@gmail.com>
2022-12-20 23:51:24 +0000

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""My Square module"""
+
+
+class Square:
+    """Define a square"""
+
+    def __init__(self, size):
+        """ Define the size of the square
+        Args:
+            size: length of a side
+        """
+
+        self.__size = size
