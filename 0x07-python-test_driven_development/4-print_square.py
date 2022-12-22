9d9fac528d5d13cb1654fa014587ee0e8fdf67e3
emmanuel tofa <emmanuelutofa@gmail.com>
2022-12-22 23:14:23 +0000

    Print square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_square(size):
+
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+    for i in range(size):
+        for j in range(size):
+            print("#", end='')
+        print()
