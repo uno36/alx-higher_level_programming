f557d67e2978c4422f15aceb83bf67eaf936c80b
Emmanuel <emmanuelutofa@gmail.com>
2023-03-13 21:07:00 +0200

    I love C

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* a script that  prints x times “C is fun */
+if (!parseInt(process.argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log('C is fun');
+  }
+}
