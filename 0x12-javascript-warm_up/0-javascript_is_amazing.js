b61f91cf4489091217de919ca8b84b6aee0565bc
emmanuel tofa <emmanuelutofa@gmail.com>
2023-03-13 19:59:57 +0000

    First constant, first print

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 /* Print a variable const */
-const myVar = "JavaScript is amazing";
-console.log(myVar);
+const { message } = { message: "JavaScript is amazing" };
+console.log(message);
