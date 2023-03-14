064de906e0df540f6049723c84ce11fbe5f68b05
Emmanuel <emmanuelutofa@gmail.com>
2023-03-14 19:54:37 +0200

    Esrever

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
-/* returns the reversed version of a list */
+// reverses a list
 exports.esrever = function (list) {
-  let list1 = [];
-  let j = list.length - 1;
-  for (let i = 0; i < list.length; i++, j--) {
-    list1[i] = list[j];
-  }
-  return list1;
+  return list.sort(() => 1);
 };
