70ec826536f1cce8508ffd8cbf2f9546a497dfc0
Emmanuel <emmanuelutofa@gmail.com>
2023-03-14 19:41:39 +0200

    Rectangle #3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,15 +1,21 @@
 #!/usr/bin/node
-/* a class Rectangle that defines a rectangle */
-module.exports = class Rectangle {
+/* Write a class Rectangle that defines a rectangle */
+
+class Rectangle {
   constructor (w, h) {
+  // Method that initialize a instance
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
+
   print () {
+  // Method that prints the rectangle using the charactere 'X'
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
-};
+}
+
+module.exports = Rectangle;
