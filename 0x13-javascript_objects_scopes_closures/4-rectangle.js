3075edd98ad1ed226f4c347cf681a5fea3853b80
Emmanuel <emmanuelutofa@gmail.com>
2023-03-14 19:47:27 +0200

    Rectangle #4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,32 +1,18 @@
 #!/usr/bin/node
-/* Write a class Rectangle that defines a rectangle */
-
-class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
-  // Method that initialize a instance
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
 
   print () {
-  // Method that prints the rectangle using the charactere 'X'
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
   }
 
   rotate () {
-  // Method that exchanges the width and the height of the rectangle}
     [this.width, this.height] = [this.height, this.width];
   }
 
   double () {
-  // Method that multiples the width and the height of the rectangle by 2
-    this.width = this.width * 2;
-    this.height = this.height * 2;
+    [this.width, this.height] = [this.width * 2, this.height * 2];
   }
-}
-
-module.exports = Rectangle;
+};
