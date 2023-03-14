db4b34cf4d13264d45dac04deb40da80f429920a
Emmanuel <emmanuelutofa@gmail.com>
2023-03-14 19:29:01 +0200

    Occurrences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* returns the number of occurrences in a list */
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      count++;
+    }
+  }
+  return count;
+};
