8a7b8c957a48f20e7aba535aec03c6f5349f823e
Emmanuel <emmanuelutofa@gmail.com>
2023-03-14 20:07:15 +0200

    Esrever

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 // defines function that returns a reversed array
-exports.esrever = function(list) {
-  var reversed = [];
-  for (var i = list.length - 1; i >= 0; i--) {
-    reversed.push(list[i]);
+exports.esrever = function (list) {
+  if (Array.isArray(list)) {
+    const reversed = Array(list.length);
+    for (let i = 0; i < list.length; i++) {
+      reversed[list.length - (i + 1)] = list[i];
+    }
+    return reversed;
   }
-  return reversed;
 };
