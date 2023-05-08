e96ed7686b960e8d12f1ea061b8646c1097e7ffa
Emmanuel <emmanuelutofa@gmail.com>
2023-05-08 17:44:33 +0200

    What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-# Python script that fetches https://intranet.hbtn.io/status
-if __name__ == "__main__":
-    import requests
+"""Fetches https://alx-intranet.hbtn.io/status."""
+import requests
 
-    r = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:\n\t- type: {}\n\t- content: {}"
-          .format(type(r.text), r.text))
+
+if __name__ == "__main__":
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
