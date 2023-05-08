f553032e9ad0772d339935a02429adabfb5cca24
Emmanuel <emmanuelutofa@gmail.com>
2023-05-08 17:27:45 +0200

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-# Python script that fetches https://intranet.hbtn.io/status
-if __name__ == "__main__":
-    import urllib.request
+"""Fetches https://alx-intranet.hbtn.io/status."""
+import urllib.request
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        content = response.read()
-        utf8_content = content.decode('utf8')
-    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
-          .format(type(content), content, utf8_content))
+
+if __name__ == "__main__":
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
