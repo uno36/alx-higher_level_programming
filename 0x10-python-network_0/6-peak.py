f344d56c2027ed48636a0c216c98e3eba2702f37
Emmanuel <emmanuelutofa@gmail.com>
2023-05-08 17:03:24 +0200

    Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python3
+""" Finds a peak in a list of unsorted integers
+"""
+
+
 def find_peak(list_of_integers):
     """
-    function that finds a peak in a list of unsorted integers.
     Args:
-        list_of_integers: list --- a list of integers
-    Return:
-        Return a peak value if found. Otherwise, return None.
+        list_of_integers(int): list of integers to find peak of
+    Returns: peak of list_of_integers or None
     """
-    mid = int(len(list_of_integers) / 2)
-    if mid - 1 >= 0 and mid + 1 <= len(list_of_integers) - 1:
-        if list_of_integers[0] > list_of_integers[1]:
-            return list_of_integers[0]
-        elif list_of_integers[len(list_of_integers) - 1] > list_of_integers[
-                len(list_of_integers) - 2]:
-            return list_of_integers[len(list_of_integers) - 1]
-        elif (list_of_integers[mid] == list_of_integers[mid - 1]) and (
-                list_of_integers[mid] == list_of_integers[mid + 1]):
-            return list_of_integers[mid]
-        elif (list_of_integers[mid] > list_of_integers[mid - 1]) and (
-                list_of_integers[mid] > list_of_integers[mid + 1]):
-            return list_of_integers[mid]
-        elif list_of_integers[mid - 1] > list_of_integers[mid + 1]:
-            return find_peak(list_of_integers[0:mid + 1])
-        elif list_of_integers[mid - 1] < list_of_integers[mid + 1]:
-            return find_peak(list_of_integers[mid: len(list_of_integers)])
-    else:
+    size = len(list_of_integers)
+    mid_e = size
+    mid = size // 2
+
+    if size == 0:
         return None
+
+    while True:
+        mid_e = mid_e // 2
+        if (mid < size - 1 and
+                list_of_integers[mid] < list_of_integers[mid + 1]):
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid + mid_e // 2
+        elif mid_e > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid - mid_e // 2
+        else:
+            return list_of_integers[mid]
