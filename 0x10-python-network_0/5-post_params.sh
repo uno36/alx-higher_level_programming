f84e44c2ee16626c937b73524d7240645cc99680
Emmanuel <emmanuelutofa@gmail.com>
2023-05-08 17:02:33 +0200

    cURL POST parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -s "$1"
+# send a POST request to the passed URL using curl, and display the body of the response
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
