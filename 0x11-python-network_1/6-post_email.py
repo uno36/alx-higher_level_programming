560f0a5873d5128fbe14afb3fd21cb7a559a15fe
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-08-26 06:58:32 +0000

    POST an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""Sends a POST request to a given URL with a given email.
-Usage: ./6-post_email.py <URL> <email>
-  - Displays the body of the response.
 """
-import sys
+    Script that takes in a URL and an email sends a POST request to that URL
+    with the email as a parameter.
+    Displays the body of the response
+"""
 import requests
+import sys
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-
-    r = requests.post(url, data=value)
-    print(r.text)
+    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    print("{}".format(r.text))
