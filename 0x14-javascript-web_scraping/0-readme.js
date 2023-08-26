d0f67008cb9ddd395a5bedd5f2f04039cf9c84a2
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-08-26 07:16:35 +0000

    Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
-/* a script that reads and prints the content of a file. */
+
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
-    console.log(data);
+    process.stdout.write(data);
   }
 });
